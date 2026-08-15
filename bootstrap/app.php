618d29cdfa15c2c7692b76308c514e15030e610d
HectorDanielQH <hdaniel369@gmail.com>
2026-08-15 15:45:58 -0400

    actualizado

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -33,3 +33,11 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*'),
         );
     })->create();
+
+$publicPath = env('APP_PUBLIC_PATH');
+
+if (is_string($publicPath) && $publicPath !== '') {
+    $app->usePublicPath($publicPath);
+}
+
+return $app;
